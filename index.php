47ebee2e79ce8d1181c49780282ecec7c12cf2bc
Yanik Peiffer <yanik-peiffer@gmx.de>
2015-10-03 18:02:14 +0200

    created content-home.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+
+<?php
+
+if(is_home()) :
+
+  get_template_part('content', 'home');
+
+else:
+
+  get_template_part('content');
+
+endif;
+
+?>
+
 <?php get_footer(); ?>
